20c6536faf0f71de2c87400df1d63f91047cef0e
Martin Achtermann <65299501+roadpilot@users.noreply.github.com>
2021-09-30 17:55:29 +0000

    final solution

--- main.sql
+++ main.sql
@@ -1,4 +1,4 @@
 /*
-problem statement
+Query the Western Longitude (LONG_W)where the smallest Northern Latitude (LAT_N) in STATION is greater than 38.7780. Round your answer to 4 decimal places.
 */
-solution
+select round(long_w,4) from station where lat_n > 38.7780 order by lat_n limit 1
